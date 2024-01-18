f2dff1d1c20274f6a134d5cf3afd4b7cc80f04ff
kishan <kishankalavadiya@gmail.com>
2024-01-18 11:11:21 +0530

    db connection correct

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_Name } from "../constants.js";
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_Name}`
+      `${process.env.MONGODB_URI}/${DB_Name}${process.env.MONGODB_URI2}`
     );
     console.log(
       `\n Mongodb connected !! DB HOST: ${connectionInstance.connection.host}`
